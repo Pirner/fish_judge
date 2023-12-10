7eb50b41de411f2633a725dc26add62a294a1658
Nerolith <stephan.pirner93@gmail.com>
2023-12-10 14:10:03 +0100

    wip - add secret loading

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+import os
+
 from flask import Flask, request, abort
 import git
 import json
+import hmac
+import hashlib
 
 
 app = Flask(__name__)
+w_secret = os.environ['WEBHOOK_SECRET']
+
+
+def is_valid_signature(x_hub_signature, data, private_key):
+    # x_hub_signature and data are from the webhook payload
+    # private key is your webhook secret
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
 
 
 @app.route('/')
@@ -15,6 +30,43 @@ def hello_world():
 def webhook():
     if request.method != 'POST':
         return 'OK'
+
+    abort_code = 418
+    # Do initial validations on required headers
+    if 'X-Github-Event' not in request.headers:
+        abort(abort_code)
+    if 'X-Github-Delivery' not in request.headers:
+        abort(abort_code)
+    if 'X-Hub-Signature' not in request.headers:
+        abort(abort_code)
+    if not request.is_json:
+        abort(abort_code)
+    if 'User-Agent' not in request.headers:
+        abort(abort_code)
+    ua = request.headers.get('User-Agent')
+    if not ua.startswith('GitHub-Hookshot/'):
+        abort(abort_code)
+    event = request.headers.get('X-GitHub-Event')
+    if event == "ping":
+        return json.dumps({'msg': 'Hi!'})
+    if event != "push":
+        return json.dumps({'msg': "Wrong event type"})
+
+    x_hub_signature = request.headers.get('X-Hub-Signature')
+    # webhook content type should be application/json for request.data to have the payload
+    # request.data is empty in case of x-www-form-urlencoded
+    if not is_valid_signature(x_hub_signature, request.data, w_secret):
+        print('Deploy signature failed: {sig}'.format(sig=x_hub_signature))
+        abort(abort_code)
+
+    payload = request.get_json()
+    if payload is None:
+        print('Deploy payload is empty: {payload}'.format(
+            payload=payload))
+        abort(abort_code)
+
+    if payload['ref'] != 'refs/heads/master':
+        return json.dumps({'msg': 'Not master; ignoring'})
 
     repo = git.Repo('/home/Nerolith/mysite/fish_judge')
     origin = repo.remotes.origin
