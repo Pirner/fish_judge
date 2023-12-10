9fb4b1aba103de124878930ef70ff8b072e0cacc
Nerolith <stephan.pirner93@gmail.com>
2023-12-10 13:54:58 +0100

    update with touch

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Fish Judge yay 2!'
+    return 'Hello from Fish Judge yay 3!'
 
 
 @app.route('/update_server', methods=['POST'])
