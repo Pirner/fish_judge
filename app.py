06e896aca5d8d012a5d9d9aae319b6305ae2abb1
Nerolith <stephan.pirner93@gmail.com>
2023-12-10 13:49:38 +0100

    test

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Fish Judge yay!'
+    return 'Hello from Fish Judge yay 1!'
 
 
 @app.route('/update_server', methods=['POST'])
